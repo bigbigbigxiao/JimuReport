789c4cc0fdfe6539b256004ce9e3384070ca18c9
bigbigbigxiao <1797108293@qq.com>
2022-03-24 11:19:19 +0800

    添加一个Test2Controller

---
+++ jimureport-example/src/main/java/com/jeecg/modules/jmreport/controller/Test2Controller.java
@@ -0,0 +1,32 @@
+package com.jeecg.modules.jmreport.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author : bigxiaoxiao
+ * @version 1.0
+ * @description : TODO
+ * @date :2022/3/24 11:18
+ */
+@RestController
+@RequestMapping("/test2")
+public class Test2Controller {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+    @Autowired
+    private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    @RequestMapping(value = "/list2", method = RequestMethod.GET)
+    public void queryPageList(HttpServletRequest req) {
+        System.out.println("getMaxRows="+jdbcTemplate.getMaxRows());
+        System.out.println("getCacheLimit="+namedParameterJdbcTemplate.getCacheLimit());
+    }
+}
